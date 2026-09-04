967b1e27d79685fa1da8da691c449fdcde0bb32e
ketansuryavanshi1509 <140500088+ketansuryavanshi1509@users.noreply.github.com>
2026-09-04 13:57:03 +0530

    Refactor app.py for improved readability and structure

--- app.py
+++ app.py
@@ -10,9 +10,9 @@ from dotenv import load_dotenv
 load_dotenv()
 
 ## Langsmith Tracking
-os.environ["LANGCHAIN_API_KEY"]=os.getenv("LANGCHAIN_API_KEY")
-os.environ["LANGCHAIN_TRACING_V2"]="true"
-os.environ["LANGCHAIN_PROJECT"]="Simple Q&A Chatbot With OPENAI"
+##os.environ["LANGCHAIN_API_KEY"]=os.getenv("LANGCHAIN_API_KEY")
+##os.environ["LANGCHAIN_TRACING_V2"]="true"
+##os.environ["LANGCHAIN_PROJECT"]="Simple Q&A Chatbot With OPENAI"
 
 ## Prompt Template
 prompt=ChatPromptTemplate.from_messages(
